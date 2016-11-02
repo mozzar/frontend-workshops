bedcc54dabe35f34b19cf50f6b68361f9af07e02
Rafał Rudol <rafal@rudol.pl>
2016-11-02 22:29:58 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+  gulp.src('public_html/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public_html/styles/'))
+});
+
+gulp.task('styles:watch',function() {
+  gulp.watch('public_html/styles/*.scss',['styles']);
+});
