4a77a18dbe4b291a23ebe38a7b8bb2279b4e7ac7
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-11-04 23:09:04 +0100

    Prepare a separate partial stylesheet for each section

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-  gulp.src('public_html/styles/*.scss')
+  gulp.src('public_html/styles/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public_html/styles/'))
 });
